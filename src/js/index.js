efb0b8c8f428195ebb6729577b64fd9035522163
Ivan Marchuk <ivan.marchuk@mail.ru>
2019-07-28 00:01:28 +0300

    added jquery, made dropdown guests and masked text field

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function() {
+
+    $('select').niceSelect();
+    $("#date").mask("99 / 99 / 9999");
+    $('#datetimepicker').datetimepicker();
+    
+});
+
--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var menuBtn = $('.dropdown-guests-button');
-    var menu = $('.dropdown-guests-list__hide');
-
-    menuBtn.on('click', function (event) {
-        event.preventDefault();
-        menu.toggleClass('dropdown-guests-list__active');
-    })
-})
