6dca2f68058dd624300c79d7f8ae00bb0beb4554
Ivan Marchuk <ivan.marchuk@mail.ru>
2019-08-04 21:56:03 +0300

    made a toggle, fixed radiobuttons

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,33 @@ $(document).ready(function() {
 
     $('select').niceSelect();
     $("#date").mask("99 / 99 / 9999");
-    $('#datetimepicker').datetimepicker();
     
-});
+    
 
+    $("input[value='male']").on('change', function (){
+        
+        if ($(this).prop('checked', true)) {
+            $("label[for='male']").toggleClass('radiobuttons-list-item__label-active');
+            $("label[for='female']").removeClass('radiobuttons-list-item__label-active');
+            
+        };
+
+       
+        
+        
+    });
+
+    $("input[value='female']").on( 'change' , function(){
+        
+        if ($("input[value='female']").prop('checked', true)){
+        
+            $("label[for='female']").toggleClass('radiobuttons-list-item__label-active');
+            $("label[for='male']").removeClass('radiobuttons-list-item__label-active');
+        };
+
+        
+
+    });
+
+
+});
