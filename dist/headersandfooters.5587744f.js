f633c88dfa6d5697cf3027982c8a68e89bf3331d
Ivan Marchuk <ivan.marchuk@mail.ru>
2019-08-10 23:29:42 +0300

    added some new widgets to headersandfooters

--- dist/headersandfooters.5587744f.js
+++ dist/headersandfooters.5587744f.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "28567" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37181" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
